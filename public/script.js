d36bdd9269e1d4c774bb0b58817a54a1dc942ee4
ruwanbandara <ruwan.se.uok@gmail.com>
2019-09-11 12:07:54 +0530

    set map API"

--- public/script.js
+++ public/script.js
@@ -1,5 +1,26 @@
 const searchElement = document.querySelector('[data-city-search]')
-const searchBox = new google.maps.places.searchBox(searchElement)
-searchBox.addListener('placer_changed',() =>{
+const searchBox = new google.maps.places.SearchBox(searchElement)
+searchBox.addListener('places_changed',() =>{
+    const place = searchBox.getPlaces()[0]
+    if (place == null) return
+    const latitude = place.geometry.location.lat()
+    const longitude = place.geometry.location.lng()
+
+    fetch('/weather',{
+        method: 'POST',
+        headers:{
+            'Content_Type': 'application/json',
+            'Accept': 'application/json'
+        },
+
+        body: JSON.stringify({
+            latitude: latitude,
+            longitude: longitude
+        })
+    }).then(res => res.json()).then(data =>{
+        console.log(data)
+        //setWeatherData(data, place.formatted_address)
+    })
+
     
 })
--- server.js
+++ server.js
@@ -11,11 +11,12 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(express.static('public'))
   
   app.post('/weather', (req, res) => {
-    const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`
-    axios({
-      url: url,
-      responseType: 'json'
-    }).then(data => res.json(data.data.currently))
+    console.log(req.body)
+     const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`
+     axios({
+       url: url,
+       responseType: 'json'
+     }).then(data => res.json(data.data.currently))
   })
   
   app.listen(3000, () => {
